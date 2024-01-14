7171545638d804e72ab9a5f78b2ccd3cfe899951
JoonHyukYoon <crayngnuk1@naver.com>
2024-01-14 20:01:16 +0900

    setting

---
+++ src/main/java/com/ll/sb20240114/Sb20240114Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb20240114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20240114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20240114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb20240114/Sb20240114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb20240114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20240114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
